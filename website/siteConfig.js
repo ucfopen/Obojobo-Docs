d58a401493394c19b5792481088502bb13b9e194
Ryan Eppers <ryan.eppers@ucf.edu>
2018-05-16 13:37:36 -0400

    updated baseUrl

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -17,7 +17,7 @@ const siteConfig = {
 	title: 'Obojobo' /* title for your website */,
 	tagline: 'Learning Objects, Reinvented',
 	url: 'https://obojobo.ucf.edu/' /* your website url */,
-	baseUrl: '/' /* base url for your project */,
+	baseUrl: '/Obojobo-Docs/' /* base url for your project */,
 	// For github.io type URLs, you would set the url and baseUrl like:
 	//   url: 'https://facebook.github.io',
 	//   baseUrl: '/test-site/',
